b21f05b1f25aaf923116a12aabedf3e08669dc48
Lucas Bonnaire <lbonnairepro@gmail.com>
2025-06-08 09:14:39 +0000

    deleted doublon register route in users/ and auth/ && reseted frontend part

--- backend/app/api/routes/user.py
+++ backend/app/api/routes/user.py
@@ -3,7 +3,7 @@ from typing import Annotated
 
 from app.crud import user as crud_user
 from app.database import get_db
-from app.schemas.user import UserCreate, UserRead
+from app.schemas.user import UserRead
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
@@ -11,20 +11,6 @@ router = APIRouter(
     prefix="/users",
     tags=["users"],
 )
-
-@router.post("/", response_model=UserRead, status_code=status.HTTP_201_CREATED)
-def create_user(
-    user_in: UserCreate,
-    db: Annotated[Session, Depends(get_db)],
-    ) -> crud_user.User:
-    """Create a new user."""
-    existing_user = crud_user.get_user_by_email(db, user_in.email)
-    if existing_user:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Email is already registered.",
-        )
-    return crud_user.create_user(db, user_in)
 
 
 @router.get("/{user_id}", response_model=UserRead)
